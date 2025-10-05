647ecd91549cb6b45a35bfe8c0c41135f91d58b0
Serhii Chernyshov <chernushov88@gmail.com>
2025-10-05 17:28:33 +0300

    chore: add Pinia to Project

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,15 @@ import App from './App.vue'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+// Pinia
+import { createPinia } from 'pinia'; 
 
+const app = createApp(App);
 const vuetify = createVuetify({
   components,
   directives,
 })
+const pinia = createPinia(); 
 
-createApp(App).use(vuetify).mount('#app')
+
+app.use(vuetify).use(pinia).mount('#app');
