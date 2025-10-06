2fd0b07652895252a686f9fd6645205ed2fb9e11
Serhii Chernyshov <chernushov88@gmail.com>
2025-10-06 12:09:19 +0300

    chore:  Add navigations

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <template>
-  <TodoList  />
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-title>Todo-test project with Vuetify</v-app-bar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn text to="/">
+        Todo List
+      </v-btn>
+      <v-btn text to="/contact">
+        Contacts
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
-
-<script setup lang="ts">
-import TodoList from './components/TDToDoList.vue'
+<script>
+export default {
+  // name: 'App',
+};
 </script>
