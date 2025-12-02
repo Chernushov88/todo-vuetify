628472783c31e412c1c4dcc33d7f4ad26008c425
Serhii Chernyshov <chernushov88@gmail.com>
2025-12-02 14:53:30 +0200

    Separete navigation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <v-app-bar-title>Todo-test project with Vuetify</v-app-bar-title>
-      <v-spacer></v-spacer>
-      <v-btn text to="/">Home</v-btn>
-      <v-btn text to="/todo">Todo List</v-btn>
-      <v-btn text to="/contact">Contacts</v-btn>
-    </v-app-bar>
+    <TDNav />
 
     <v-main>
       <router-view></router-view>
@@ -14,8 +8,6 @@
   </v-app>
 </template>
 
-<script>
-export default {
-  // name: 'App',
-};
+<script setup>
+import TDNav from './components/TDNav.vue';
 </script>
---
+++ src/components/TDNav.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app-bar app color="primary" dark>
+      <v-app-bar-title>Chernyshov</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <template v-if="auth.isLoggedIn">
+        <v-btn text to="/">Home</v-btn>
+        <v-btn text to="/todo">Todo List</v-btn>
+        <v-btn text to="/contact">Contacts</v-btn>
+        <v-btn text-red @click="auth.logout()">Logout</v-btn>
+      </template>  
+      
+    </v-app-bar>
+</template>
+
+<script setup>
+import { useAuthStore } from '@/stores/auth'
+const auth = useAuthStore()
+</script>
